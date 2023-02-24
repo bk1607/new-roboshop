1cf515c988cdd18c03886674c509c979bd8f1882
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-02-24 23:31:02 +0530

    catalogue

--- common.sh
+++ common.sh
@@ -6,11 +6,88 @@ log_file=/tmp/roboshop.org
 code_dir=$(pwd)
 
 error_check() {
-  if [ $1 -eq 0 ];then
+  if [ "$1" -eq 0 ];then
     echo "SUCCESS"
   else
     echo "Failure"
     echo "something went wrong check ${log_file} for error"
     exit 1
   fi
+}
+
+systemd_setup(){
+  print_head "setting up system files"
+  cp "${code_dir}"/configs/"${component}".service /etc/systemd/system/"${component}".service &>>"${log_file}"
+  error_check $?
+
+  print_head "loading service"
+  systemctl daemon-reload &>>"${log_file}"
+  error_check $?
+
+  print_head "starting catalogue service"
+  systemctl enable "${component}" &>>"${log_file}"
+  systemctl restart "${component}" &>>"${log_file}"
+
+}
+
+app_setup(){
+
+  print_add "creating an application user"
+  id roboshop &>>"${log_file}"
+  if [ $? -ne 0 ];then
+    useradd roboshop &>>"${log_file}"
+  fi
+  error_check $?
+
+  print_head "creating app directory"
+  if [ ! -d /app ];then
+    mkdir /app &>>"${log_file}"
+  elif [ -d /app ];then
+    rm -rf /app &>>"${log_file}"
+  fi
+  error_check $?
+
+  print_head "downloading application to app directory"
+  curl -L -o /tmp/"${component}".zip https://roboshop-artifacts.s3.amazonaws.com/"${component}".zip &>>"${log_file}"
+  cd /app
+  unzip /tmp/"${component}".zip &>>"${log_file}"
+
+}
+
+node_js(){
+  print_head "setup nodejs repo file"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>"${log_file}"
+  error_check $?
+
+  print_head "installing nodejs"
+  yum install nodejs -y &>>"${log_file}"
+  error_check $?
+
+  app_setup
+
+  print_head "installing dependencies"
+  cd /app
+  npm install &>>"${log_file}"
+
+  schema_setup
+
+  systemd_setup
+
+
+}
+
+schema_setup(){
+  if [ "${schema}"=='mongo' ];then
+    print_head "creating mongodb repo file"
+    cp "${code_dir}"/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>"${log_file}"
+    error_check $?
+
+    print_head "installing mongodb"
+    yum install mongodb-org-shell -y &>>"${log_file}"
+    error_check $?
+
+    print_head "loading schema"
+    mongo --host mongodb.devops2023.online </app/schema/catalogue.js
+  fi
+
 }
