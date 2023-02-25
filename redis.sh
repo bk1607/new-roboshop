801305e235208546de402697215078c9cd630166
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-02-25 18:44:38 +0530

    0.0.0.0

--- redis.sh
+++ redis.sh
@@ -12,7 +12,7 @@ yum install redis -y &>>"${log_file}"
 error_check $?
 
 print_head "allowing all the incoming traffic"
-sed -i -e 's/127.0.0.1/0.0.0./' /etc/redis.conf /etc/redis/redis.conf &>>"${log_file}"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>"${log_file}"
 error_check $?
 
 print_head "enabling and restarting the service"
